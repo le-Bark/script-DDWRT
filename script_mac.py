98fd14f50aecef8f19904e8e7211cf3455fc6bcd
Marc-Andre <marcadenis@msn.com>
2018-11-07 04:11:32 +0100

    correction string

--- script_mac.py
+++ script_mac.py
@@ -3,7 +3,7 @@ import re
 str = subprocess.check_output(["wl","-a","eth1","assoclist"])
 #str = "assoclist 8C:F5:A3:42:62:9B\nassoclist AC:5F:3E:B3:3C:8B\n"
 
-matchList = re.findall(r"(?:[A-F0-9]{2}:){5}[A-F0-9]{2}",str,0)
+matchList = re.findall(r"(?:[A-F0-9]{2}:){5}[A-F0-9]{2}",str.decode("utf-8"),0)
 
 
 if matchList :
