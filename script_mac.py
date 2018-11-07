f310778b2aec5a64bcaff61ae9bb162a84e94a2b
bark <marcadenis@msn.com>
2018-11-06 22:05:47 -0500

    ajout du regex

--- script_mac.py
+++ script_mac.py
@@ -1,7 +1,18 @@
 import subprocess
-
+import re
 str = subprocess.check_output(["wl","-a","eth1","assoclist"])
+#str = "assoclist 8C:F5:A3:42:62:9B\nassoclist AC:5F:3E:B3:3C:8B\n"
 
-print (str)
-print("test")
+matchList = re.findall(r"(?:[A-F0-9]{2}:){5}[A-F0-9]{2}",str,0)
+
+
+if matchList :
+	print("match")
+else:
+	print("no match")
+	
+print("\n")
+
+print(matchList)
+
 
